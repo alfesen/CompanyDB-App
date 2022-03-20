98ca1c42700802ffde95b87c639df2a601d7b0af
alfesen <89510380+alfesen@users.noreply.github.com>
2022-03-20 16:45:24 +0100

    Populate() refuses to cooperate

--- routes/employees.routes.js
+++ routes/employees.routes.js
@@ -4,7 +4,7 @@ const Employee = require('../models/employee.model');
 
 router.get('/employees', async (req, res) => {
   try {
-    res.json(await Employee.find());
+    res.json(await Employee.find().populate('departments'));
   }
   catch (err) {
     res.status(500).json({message: err});
@@ -13,7 +13,7 @@ router.get('/employees', async (req, res) => {
 
 router.get('/employees/random', async (req, res) => {
   try {
-    const count = await Employee.countDocument();
+    const count = await Employee.countDocuments();
     const rand = Math.floor(Math.random() * count);
     const emp = await Employee.findOne().skip(rand);
     if (!emp) {
