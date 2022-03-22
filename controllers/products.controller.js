b85f690bbc87da73d32ad8b9baafda3f8a6106f1
alfesen <89510380+alfesen@users.noreply.github.com>
2022-03-22 17:38:06 +0100

    Spread routes in containers

---
+++ controllers/products.controller.js
@@ -0,0 +1,81 @@
+const Product = require('../models/product.model');
+
+exports.getAll = async (req, res) => {
+  try{
+    res.json(await Product.find());
+  }
+  catch (err) {
+    res.status(500).json({message: err});
+  }
+};
+
+exports.getRandom = async (req, res) => {
+  try {
+    const count = await Product.countDocuments();
+    const rand = Math.floor(Math.random() * count);
+    const prod = await Product.findOne().skip(rand);
+    if (!prod) res.status(404).json({message: 'NotFound'});
+    else res.json(prod);
+  }
+  catch (err) {
+    res.status(500).json({message: err});
+  }
+};
+
+exports.getById = async (req, res) => {
+  try {
+    const prod = await Product.findById(req.params.id);
+    if (!prod) res.status(404).json({ message: 'Not found' });
+    else res.json(prod);
+  }
+  catch (err) {
+    res.status(500).json({message: err});
+  }
+};
+
+exports.postProd = async (req, res) => {
+  const {
+    name,
+    client
+  } = req.body;
+  try {
+    const newProduct = new Product ({name: name, client: client});
+    await newProduct.save();
+    res.json({message: 'OK'});
+  }
+  catch (err) {
+    res.status(500).json({message: err});
+  }
+};
+
+exports.updateProd = async (req, res) => {
+  const {
+    name,
+    client
+  } = req.body;
+  try {
+    const prod = await Product.updateOne({ _id: req.params.id }, { $set: { name: name, client: client } });
+    if (prod) {
+      prod.name = name;
+      prod.client = client;
+      await prod.save();
+      res.json({message: 'OK'});
+    } else res.status(404).json({ message: 'Not Found' });
+  }
+  catch (err) {
+    res.status(500).json({message: err});
+  }
+};
+
+exports.deleteProd = async (req, res) => {
+  try {
+    const prod = await Product.findById(req.params.id);
+    if (prod) {
+      await Product.deleteOne({ _id: req.params.id });
+      res.json({message: 'OK'}); 
+    } else res.status(404).json({ message: 'Not Found '});
+  }
+  catch {
+    res.status(500).json({message: err});
+  }
+}
